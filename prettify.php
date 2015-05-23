c1afea818dfe8c7e85d09e4eeb262404b6e6179a
Googlo <Googlo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-23 02:38:24 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/g-prettify/trunk@1166069 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prettify.php
+++ prettify.php
@@ -3,13 +3,13 @@
 Plugin Name: G-prettify
 Plugin URI: http://googlo.me/archives/3255.html
 Description: 乐趣公园代码高亮插件,本插件只在文本环境下有效，点击按钮，然后粘贴代码即可，兼容基于google-code-prettify代码高亮的插件&主题
-Version: 2.0
+Version: 3.0
 Author: 云落
 Author URI: http://googlo.me/
 */
 //首先头部加载样式
 function prettify_head() {
-    echo '<style type="text/css">.prettyprint,pre.prettyprint{background-color:#272822;border:none;white-space: pre-wrap;word-wrap: break-word;padding:10px 15px;}.prettyprint.linenums,pre.prettyprint.linenums{-webkit-box-shadow:inset 40px 0 0 #39382E,inset 41px 0 0 #464741;-moz-box-shadow:inset 40px 0 0 #39382E,inset 41px 0 0 #464741;box-shadow:inset 40px 0 0 #39382E,inset 41px 0 0 #464741;}.prettyprint.linenums ol,pre.prettyprint.linenums ol{margin:0 0 0 33px;}.prettyprint.linenums ol li,pre.prettyprint.linenums ol li{padding-left:12px;color:#bebec5;line-height:20px;margin-left:0;list-style:decimal;}.prettyprint .com{color:#93a1a1;}.prettyprint .lit{color:#AE81FF;}.prettyprint .pun,.prettyprint .opn,.prettyprint .clo{color:#F8F8F2;}.prettyprint .fun{color:#dc322f;}.prettyprint .str,.prettyprint .atv{color:#E6DB74;}.prettyprint .kwd,.prettyprint .tag{color:#F92659;}.prettyprint .typ,.prettyprint .atn,.prettyprint .dec,.prettyprint .var{color:#A6E22E;}.prettyprint .pln{color:#66D9EF;}</style>';
+    echo '<style type="text/css">.prettyprint,pre.prettyprint{background-color:#444;border:1px solid #272822;overflow:hidden;padding:0;margin:20px 0;font:14px/20px "courier new";color:#666;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px}.prettyprint.linenums,pre.prettyprint.linenums{-webkit-box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741;-moz-box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741;box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741}.prettyprint.linenums ol,pre.prettyprint.linenums ol{margin:0 0 0 33px}.prettyprint.linenums ol li,pre.prettyprint.linenums ol li{color:#bebec5;line-height:20px;margin-left:0;list-style:decimal}.prettyprint ol.linenums{margin-bottom:0;background-color:#272822;padding:8px}.prettyprint .com{color:#93a1a1}.prettyprint .lit{color:#ae81ff}.prettyprint .pun,.prettyprint .opn,.prettyprint .clo{color:#f8f8f2}.prettyprint .fun{color:#dc322f}.prettyprint .str,.prettyprint .atv{color:#e6db74}.prettyprint .kwd,.prettyprint .tag{color:#f92659}.prettyprint .typ,.prettyprint .atn,.prettyprint .dec,.prettyprint .var{color:#a6e22e}.prettyprint .pln{color:#66d9ef}</style>';
 }
 add_action('wp_head', 'prettify_head');
 //防止代码转义
@@ -29,9 +29,9 @@ function g_prettify_esc_callback($matches)
 add_filter('the_content', 'g_prettify_esc_html', 2);
 add_filter('comment_text', 'g_prettify_esc_html', 2);
 
-//强制兼容<pre>以及<pre class="prettyprint linenums" >
+//强制兼容<pre>
 function g_prettify_replace($text){
-	$replace = array( '<pre>' => '<pre class="prettyprint" >','<pre class="prettyprint linenums" >' => '<pre class="prettyprint" >' );
+	$replace = array( '<pre>' => '<pre class="prettyprint" >' );
 	$text = str_replace(array_keys($replace), $replace, $text);
 	return $text;
 }
@@ -49,7 +49,7 @@ add_action('get_footer','prettify');
 function prettify_bottom($mce_settings) {
 ?>
 <script type="text/javascript">
-QTags.addButton( 'kkpre', '代码高亮', '<pre class="prettyprint" >\n\n</pre>', "" );//添加高亮代码
+QTags.addButton( 'kkpre', 'G-Prettify', '<pre class="prettyprint linenums" >\n\n</pre>', "" );//添加高亮代码
 QTags.addButton( 'i</>', '</>', "&lt;", "&gt;" );
 function prettify_bottom() {
 }
--- readme.txt
+++ readme.txt
@@ -3,15 +3,17 @@ Contributors: 云落
 Donate link: http://googlo.me
 Tags: 代码高亮, prettify, code, highlight
 Requires at least: 3.5
-Tested up to: 4.1
+Tested up to: 4.2
 Stable tag: 1.0.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-WordPress最简单，最小巧的代码高亮插件
+WordPress最简单，最小巧的代码高亮插件/WordPress simplest, most compact, preferably with code highlighting plug
 
 == Description ==
-WordPress最简单，最小巧，最好用的代码高亮插件，只需要一个pre标签就可以搞定一切，兼容所有的标准的google-code-prettify 代码高亮方案
+WordPress simplest, most compact, preferably with code highlighting plug, only one can get everything pre labels, compatible with all standard google-code-prettify code highlighting scheme, this plug-in HTML editor environment only by using the visual editor user, do not use! ! !
+=================下面是中文========================
+WordPress最简单，最小巧，最好用的代码高亮插件，只需要一个pre标签就可以搞定一切，兼容所有的标准的google-code-prettify 代码高亮方案，本插件仅限在HTML编辑器环境下使用，可视化编辑器使用者勿用！！！
 == Installation ==
 
 1，下载(Download)
