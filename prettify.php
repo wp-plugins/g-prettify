347e763c670d2830eb522b1e9697c01ce2deba81
Googlo <Googlo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-26 04:52:04 +0000

    修复了一些问题
    git-svn-id: https://plugins.svn.wordpress.org/g-prettify/trunk@1167499 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- prettify.php
+++ prettify.php
@@ -3,13 +3,13 @@
 Plugin Name: G-prettify
 Plugin URI: http://googlo.me/archives/3255.html
 Description: 乐趣公园代码高亮插件,本插件只在文本环境下有效，点击按钮，然后粘贴代码即可，兼容基于google-code-prettify代码高亮的插件&主题
-Version: 3.0
+Version: 4.0
 Author: 云落
 Author URI: http://googlo.me/
 */
 //首先头部加载样式
 function prettify_head() {
-    echo '<style type="text/css">.prettyprint,pre.prettyprint{background-color:#444;border:1px solid #272822;overflow:hidden;padding:0;margin:20px 0;font:14px/20px "courier new";color:#666;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px}.prettyprint.linenums,pre.prettyprint.linenums{-webkit-box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741;-moz-box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741;box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741}.prettyprint.linenums ol,pre.prettyprint.linenums ol{margin:0 0 0 33px}.prettyprint.linenums ol li,pre.prettyprint.linenums ol li{color:#bebec5;line-height:20px;margin-left:0;list-style:decimal}.prettyprint ol.linenums{margin-bottom:0;background-color:#272822;padding:8px}.prettyprint .com{color:#93a1a1}.prettyprint .lit{color:#ae81ff}.prettyprint .pun,.prettyprint .opn,.prettyprint .clo{color:#f8f8f2}.prettyprint .fun{color:#dc322f}.prettyprint .str,.prettyprint .atv{color:#e6db74}.prettyprint .kwd,.prettyprint .tag{color:#f92659}.prettyprint .typ,.prettyprint .atn,.prettyprint .dec,.prettyprint .var{color:#a6e22e}.prettyprint .pln{color:#66d9ef}</style>';
+    echo '<style type="text/css">.prettyprint,pre.prettyprint{white-space:pre-wrap;word-wrap:break-word;background-color:#444;border:1px solid #272822;overflow:hidden;padding:0;margin:20px 0;font:14px/20px "courier new";color:#666;-webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px}.prettyprint.linenums,pre.prettyprint.linenums{-webkit-box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741;-moz-box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741;box-shadow:inset 40px 0 0 #39382e,inset 41px 0 0 #464741}.prettyprint.linenums ol,pre.prettyprint.linenums ol{margin:0 0 0 33px}.prettyprint.linenums ol li,pre.prettyprint.linenums ol li{color:#bebec5;line-height:20px;margin-left:0;list-style:decimal}.prettyprint ol.linenums{margin-bottom:0;background-color:#272822;padding:0}.prettyprint .com{color:#93a1a1}.prettyprint .lit{color:#ae81ff}.prettyprint .pun,.prettyprint .opn,.prettyprint .clo{color:#f8f8f2}.prettyprint .fun{color:#dc322f}.prettyprint .str,.prettyprint .atv{color:#e6db74}.prettyprint .kwd,.prettyprint .tag{color:#f92659}.prettyprint .typ,.prettyprint .atn,.prettyprint .dec,.prettyprint .var{color:#a6e22e}.prettyprint .pln{color:#66d9ef}</style>';
 }
 add_action('wp_head', 'prettify_head');
 //防止代码转义
